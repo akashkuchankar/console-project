9291402176ecdd94c07e33c382305511a2bc4a47
akashkuchankar <kuchankarakash@gmail.com>
2022-09-30 02:15:45 -0700

    Add project files.

---
+++ console project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine(sum);
+            int sub = a - b;
+            Console.WriteLine(sub);
+            int mul = a * b;
+            Console.WriteLine(mul);
+            int div = a / b;
+            Console.WriteLine(div);
+            int mode = a% b;
+            Console.WriteLine(mode);
+
+        }
+    }
+}
